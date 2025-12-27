fae954cde1610d883c24a81848a2b8f9b706941e
Rhcoder79 <robiulhasan7972171@gmail.com>
2025-12-27 21:29:25 +0600

    banner section complete

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    {/* <h1>Vite + React</h1> */}
+    <nav className=' flex   justify-around mt-4 items-center'>
+      <div><h1 className='font-bold text-2xl'>CS — Ticket System</h1></div>
+      <div className='flex   items-center gap-4 text-[#000000] opacity-90'>
+        <div>Home</div>
+        <div>FAQ</div>
+        <div>Changelog</div>
+        <div>Blog</div>
+        <div>Download</div>
+        <div>Contact</div>
+        <div><button className='btn btn-primary '> + New Ticket</button> </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </nav>
+    <div className='banner max-w-[90%] font-bold mx-auto text-white flex justify-center gap-4 bg-gray-400 mt-4'>
+      <div className=" w-[40%] p-10 text-center rounded-xl "
+      style={{backgroundImage:`url('/images/vector1.png'),linear-gradient(to right,#632ee3,#9f62f2)`}}>
+        <p className='text-2xl'>In-progress</p>
+        <h1 className='text-3xl'>0</h1>
+        </div>
+      <div className=" w-[40%] p-10 text-center rounded-xl"
+       style={{backgroundImage:`url('/images/vector1.png'),linear-gradient(to right,#54CF68,#00827A)`}}>
+        <p className='text-2xl' >Resolved</p>
+        <h1 className='text-3xl' >0</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+
+      
     </>
   )
 }
