253cfa186229bf46fe2160450ace7dc103f09893
Rhcoder79 <robiulhasan7972171@gmail.com>
2025-12-28 23:50:40 +0600

    design complete

--- template/src/Components/Footer/Footer.jsx
+++ template/src/Components/Footer/Footer.jsx
@@ -1,34 +1,58 @@
 import React from 'react';
-
+import X from "../../assets/x.png"
+import Facebook from "../../assets/facebook.png"
+import Linkedin from "../../assets/linkedin.png"
+import Gmail from "../../assets/gmail.png"
 const Footer = () => {
     return (
-        <footer className="bg-[#09090B] p-10">
-   <nav>
-    <h3>CS — Ticket System</h3>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        <footer className="bg-[#09090B] p-10  ">
+   <section className='grid grid-cols-5 gap-5 text-white max-w-[90%] mx-auto border-b-2 border-gray-300'>
+    <nav >
+    <h3 className='font-bold'>CS — Ticket System</h3>
+    <p className='opacity-70'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
          when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
    </nav>
-   <nav>
-   <h6>Company</h6>
-   <li>About Us</li>
+   <nav >
+   <h6 className='font-bold' >Company</h6>
+   <ul className='opacity-70'>
+    <li>About Us</li>
    <li>Our Mission</li>
-   <li>Contact Saled</li>
+   <li>Contact Sale</li>
+   </ul>
+   </nav>
+   <nav >
+    <h6 className='font-bold'>Services</h6>
+    <ul className='opacity-70'>
+        <li>Products & Services</li>
+    <li>Customer Stories</li>
+    <li>Download Apps</li>
+    </ul>
    </nav>
    <nav>
-    <h6>Information</h6>
+    <h6 className='font-bold'>Information</h6>
+    <ul className='opacity-70'>
+     <li>Privacy Policy</li>
     <li>Terms & Conditions</li>
     <li>Join Us</li>  
+    </ul>
    </nav>
-   <nav>
-    <h6>Social Links</h6>
-    <li>@CS — Ticket System</li>
-    <li>@CS — Ticket System</li>
-    <li>@CS — Ticket System</li>
-    <li>support@cst.com</li>
+   <nav >
+    <h6 className='font-bold'>Social Links</h6>
+    <ul className='opacity-70'>
+    <li className='flex gap-2 items-center'><img src={X} alt="" /> @CS — Ticket System</li>
+    <li className='flex gap-2 items-center' ><img className='w-4 h-4' src={Linkedin} alt="" /> @CS — Ticket System</li>
+    <li className='flex gap-2 items-center'><img src={Facebook} alt="" /> @CS — Ticket System</li>
+    <li className='flex gap-2 items-center'><img src={Gmail} alt="" /> support@cst.com</li>
+    </ul>
    </nav>
+   <br />
+   </section>
+   <br />
+   <div className='text-center text-white opacity-90'><p>© 2025 CS — Ticket System. All rights reserved.</p></div>
 </footer>
     );
 };
 
 export default Footer;
+
